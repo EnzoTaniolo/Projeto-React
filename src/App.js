2d7d6d9d6923e91123292df2b4cab2dec2e160ab
EnzoTaniolo <enzotaniolofort@gmail.com>
2023-08-29 17:05:52 -0300

    Página Principal estilizada

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react"
 
+import People from './assets/people.svg'
+import Arrow from './assets/arrow.svg'
+
 import { Container, Image, ContainerItens, H1, InputLabel, Input, Button } from "./styles.js"
 
 //JSX
 function App() {
     return (
     <Container>
-        <Image src="" alt="" />
+        <Image alt="logo-image" src={People}/>
 
         <ContainerItens>
             <H1>Olá!</H1>
@@ -17,7 +20,7 @@ function App() {
             <InputLabel>Idade</InputLabel>
             <Input type="number" placeholder="Idade"/>
 
-            <Button>Cadastrar</Button>
+            <Button>Cadastrar <img alt="arrow" src={Arrow}/></Button>
         </ContainerItens>
     </Container>
     );
